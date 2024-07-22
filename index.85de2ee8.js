ff9ad3b3d941ee875566bccac0273044792ef0a2
Denys Khvashchenko <khvashchenkodenys.gmail.com>
2024-07-22 18:48:14 +0300

    make build

---
+++ index.85de2ee8.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var l=document.createElement("li");r.appendChild(l),l.textContent=a,Object.keys(t[a]).length>0&&e(l,t[a])}n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.85de2ee8.js.map
